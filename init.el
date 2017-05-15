694ca3a927872ed3ec4633fb9cdfa605cd90701a
ansumanbebarta <ansuman.bebarta@gmail.com>
2017-05-15 16:56:05 +0530

    Rewrite init.el
    1. Add proper comments
    2. Add proper documentation
    3. Format properly

--- init.el
+++ init.el
@@ -1,92 +1,139 @@
-;;--------------------------------------------------------------------
-;; Package initializations
-;;--------------------------------------------------------------------
+;;; init.el --- Ansuman Bebarta's Emacs init file for GNU Emacs.
 
+;; Description: Emacs init file
+;; Author: Ansuman Bebarta <ansuman.bebarta@gmail.com>
+;; Maintainer: Ansuman Bebarta <ansuman.bebarta@gmail.com>
+;; Keyword: local, convenience
+
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included
+;; in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentary:
+
+;; This is my personal startup file for GNU Emacs.  It has only recently
+;; been tested on GNU Emacs 24.3.1, though it may run on other versions.
+
+;;; Code:
+
+;; Personal information
+;; --------------------
+
+(setq user-full-name "Ansuman Bebarta"
+      user-mail-address "ansuman.bebarta@gmail.com")
+
+;; Package Management
+;; ------------------
+
+(require 'cl)
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("elpy" . "https://jorgenschaefer.github.io/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-(defvar myPackages
+;; Define default packages
+;; -----------------------
+
+(defvar required-packages
   '(
     ace-window
-    auto-complete
+    key-chord
+    exec-path-from-shell
+    linum-off
+
+    ;; Theme
     base16-theme
-    ;; Use to change minor mode look on modeline
+    spaceline
     diminish
-    ;; Emacs IPython Notebook
-    ein
-    ;; Emacs rpc (explore later)
-    ;; epc
-    elpy
+    fancy-battery
+
+    ;; Evil
     evil
     evil-magit
-    evil-matchit
     evil-leader
-    ;; Gather PATH from shell
-    exec-path-from-shell
-    ;; Used to show battery info in modeline
-    fancy-battery
-    ;; Realtime syntax checking
-    flycheck
+
+    ;; Project
     helm
     helm-projectile
-    jedi
-    ;; Allows to bind commands to combination of keys
-    key-chord
-    linum-off
-    ;; Explore later
-    ;; paradox
-    ;; Provide many workspaces (explore later)
     perspective
     projectile
     project-explorer
-    ;; Checks pep8
-    py-autopep8
-    spaceline
+
+    ;; Syntax
+    flycheck
+
+    ;; Python
+    elpy
+    jinja2-mode
     virtualenvwrapper
+
+    ;; Yaml
     yaml-mode
-    ))
+    )
+  "A list of packages used.")
 
-(mapc #'(lambda (package)
-	  (unless (package-installed-p package)
-	    (package-install package)))
-      myPackages)
+;; Install packages
+;; ----------------
 
-;;--------------------------------------------------------------------
-;; Basic customization
-;;--------------------------------------------------------------------
+(defun packages-installed-p ()
+  "Check whether all required packages all installed or not."
+  (loop for package in required-packages
+	when (not (package-installed-p package))
+	do (return nil)
+	finally (return t)))
 
-;; Answer with y and n
-(defalias 'yes-or-no-p 'y-or-n-p)
+(unless (packages-installed-p)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
 
-;; Make alias for encoding
-(define-coding-system-alias 'UTF-8 'utf-8)
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
-;; Language
+;; Start up options
+;; --------------------
+
 (setq current-language-environment "English")
 
-;; Don't show startup screen
-(setq inhibit-startup-screen t)
+(setq inhibit-splash-screen t
+      initial-scratch-message nil)
 
-;; Set scratch message to empty string
-(setq initial-scratch-message "")
-
-;; Don't show the menu bar
+; No scroll, tool or menu.
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 (menu-bar-mode -1)
 
-;; Don't show the tool bar
-(tool-bar-mode -1)
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq select-enable-clipboard t)
 
-;; Don't show scroll bar
-(toggle-scroll-bar -1)
+; No need of back ups.
+(setq make-backup-files nil)
 
-;; Don't blink the cursor
+; Display settings.
 (blink-cursor-mode -1)
+(set-fringe-mode 0)
+(show-paren-mode t)
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines) (toggle-indicate-empty-lines))
 
-;; Show line number on left and column number on mode line
+; Show side line
 (global-linum-mode t)
 (if (display-graphic-p)
     (setq linum-format "%5d \u2502 ")
@@ -96,103 +143,43 @@
 (setq line-number-display-limit-width 10000)
 (column-number-mode t)
 
-;; Use space instead of tabs
-(setq indent-tabs-mode -1)
+; Indentation and spaces for tabs.
+(setq indent-tabs-mode nil
+      tab-width 4)
 
-;; Ignore case while searching
+; Ignore case while searching.
 (setq case-fold-search t)
 
-;; Require final newlines in files when they are saved
+; Misc settings.
+(defalias 'yes-or-no-p 'y-or-n-p)
+(define-coding-system-alias 'UTF-8 'utf-8)
 (setq require-final-newline t)
-
-;; Remove trailing whitespace before save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Abstract back files and autosave files
-(setq backup-directory-alist '((".*" . "~/.emacs.d/backup")))
-(setq version-control t)
-(setq delete-old-versions t)
 (setq auto-save-list-file-prefix "~/.emacs.d/autosave/")
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)))
 
-;; recentf-mode allows you to access the list of recent files which can be
-;; used by ido and helm. Let’s save its file somewhere else and change the
-;; size of its history while we’re at it.
+; recentf-mode allows you to access the list of recent files which can be
+; used by ido and helm. Let’s save its file somewhere else and change the
+; size of its history while we’re at it.
 (setq recentf-save-file "~/.emacs.d/etc/recentf"
       recentf-max-saved-items 50)
 
-;; The history of prompts like M-: can be saved,
-;; but let’s change its save file and history length first.
+; The history of prompts like M-: can be saved,
+; but let’s change its save file and history length first.
 (setq savehist-file "~/.emacs.d/etc/savehist"
       history-length 150)
 
-;; Start from the last place you were in a file the next time you visit
-;; Put save file somewhere else
+; Start from the last place you were in a file the next time you visit
+; Put save file somewhere else
 (setq-default save-place t)
 (setq save-place-file "~/.emacs.d/etc/saveplace")
 
-;; Put bookmarks somewhere else
+; Put bookmarks somewhere else
 (setq bookmark-default-file "~/.emacs.d/etc/bookmarks")
 
-;; Don't need fringe
-(set-fringe-mode 0)
-
-;; Setting font to monaco
-(if (member "Monaco" (font-family-list))
-    (set-face-attribute
-     'default nil :font "Monaco 14"))
-
-;;--------------------------------------------------------------------
-;; Custom functions and definations
-;;--------------------------------------------------------------------
-
-(defun find-user-init-file ()
-  "Edit the `user-init-file', in another window."
-  (interactive)
-  (find-file user-init-file))
-
-;; Fix for ansi term "4m" issue in mac
-;; http://stackoverflow.com/questions/8918910/weird-character-zsh-in-emacs-terminal/8920373#8920373
-;; Fix for zsh to play nice in ansi-term in linux
-(if (eq system-type 'gnu/linux)
-    (add-hook 'term-exec-hook
-          (function
-           (lambda ()
-	     (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)))))
-
-(defun term-toggle-mode ()
-  (interactive)
-  (if (term-in-line-mode)
-      (term-char-mode)
-    (term-line-mode)))
-
-(defun start-zsh-ansi-term ()
-  "User zsh as default shell for ansi-term."
-  (interactive)
-  (ansi-term "/bin/bash"))
-
-;; Adding hook to close buffer of ansi-term
-(defun close-ansi-term-buffer-on-exit ()
-  (let* ((buff (current-buffer))
-         (proc (get-buffer-process buff)))
-    (set-process-sentinel
-     proc
-     `(lambda (process event)
-        (if (string= event "finished\n")
-            (kill-buffer ,buff))))))
-(add-hook 'term-exec-hook 'close-ansi-term-buffer-on-exit)
-
-(defun activate-venv (name)
-  "Activates virtualenv with given name"
-  (venv-workon name))
- 
-(defun current-persp-name ()
-  "Return current perspective name"
-  (persp-name persp-curr))
-
-;;--------------------------------------------------------------------
-;; Theme and modeline setup
-;;--------------------------------------------------------------------
+;; Theme
+;; -----
 
 (defun apply-theme()
   "Apply theme."
@@ -210,69 +197,59 @@
 (if (eq system-type 'darwin)
     (setq ns-use-srgb-colorspace nil))
 
-;; Theme
 (if (display-graphic-p)
     (apply-theme))
 
-;;--------------------------------------------------------------------
-;; Package configuations
-;;--------------------------------------------------------------------
+;; Font
+;; ----
+(if (member "Monaco" (font-family-list))
+    (set-face-attribute
+     'default nil :font "Monaco 14"))
 
-;; Gather PATH from shell as GUI emacs don't do by default
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+;; Custom functions
+;; ----------------
 
-;; Ace window
-(require 'ace-window)
+(defun find-user-init-file ()
+  "Edit the EMACS init file in another window."
+  (interactive)
+  (find-file user-init-file))
 
-;; Linum off mode
-(require 'linum-off)
+(defun activate-venv (name)
+  "Activate virtualenv with given NAME."
+  (venv-workon name))
 
-;; Evil-leader-config
+(defun current-persp-name ()
+  "Return current perspective name."
+  (persp-name persp-curr))
+
+;; Evil
+;; ----
+
 (require 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
 
-;; Evil-config
 (require 'evil)
 (evil-mode 1)
 
-;; Evil-magit-config
 (require 'evil-magit)
 
-;; Evil-matchit
-(require 'evil-matchit)
-(global-evil-matchit-mode 1)
+;; Project setup
+;; -------------
 
-;; Key chord
-(key-chord-mode 1)
-
-;; Helm-config
 (require 'helm-config)
 (helm-mode t)
 (setq helm-mode-fuzzy-match t)
 (setq helm-completion-in-region-fuzzy-match t)
 
-;; Projectile-config
 (projectile-global-mode)
 (setq projectile-enable-caching t)
 
-;; Perspective-config
 (persp-mode)
 
-;; Helm-projectile-config
 (require 'helm-projectile)
 (helm-projectile-on)
 
-;; Auto-complete
-(ac-config-default)
-(setq ac-use-quick-help -1)
-(add-hook 'python-mode-hook 'jedi:setup)
- 
-;; Flycheck
-(global-flycheck-mode)
- 
-;; TODO: Check for renaming of perspective
 (add-hook 'persp-created-hook
       '(lambda ()
          (activate-venv (current-persp-name))))
@@ -280,26 +257,12 @@
       '(lambda ()
          (activate-venv (current-persp-name))))
 
-;; Add support for yaml
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+; Flycheck
+(global-flycheck-mode)
 
-;;--------------------------------------------------------------------
-;; Custom keys
-;;--------------------------------------------------------------------
+;; Key configurations
+;; ------------------
 
-;; Use up and down arrows to go up and down in history in ipython
-(define-key comint-mode-map (kbd "<up>")
-      'comint-previous-input)
-(define-key comint-mode-map (kbd "<next>")
-      'comint-next-input)
-
-;; Escape to normal mode with 'jk' in evil-mode
-(setq key-chord-two-keys-delay 0.5)
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-
-;; Key configuration with evil-leader
 (evil-leader/set-key
   "I" 'find-user-init-file
   "c" 'comment-or-uncomment-region
@@ -313,26 +276,17 @@
   "bR" 'revert-buffer
   "bs" 'persp-switch-to-buffer
 
-  ;; j stands for jedi
-  "jd" 'jedi:goto-definition
-  "jD" 'jedi:get-in-function-call
-
   ;; p stands for project
   "pp" 'helm-projectile-switch-project
   "pf" 'helm-projectile-find-file
   "pg" 'helm-projectile-grep
   "pi" 'projectile-invalidate-cache
 
-  ;; s stands for shell
-  "ss" 'start-zsh-ansi-term
-  "st" 'term-toggle-mode
-
   ;; t stands for tree
   "tt" 'project-explorer-toggle
 
   ;; w stands for workspace
   "ws" 'persp-switch
-  "wr" 'persp-rename
   "wk" 'persp-kill
   "wl" 'persp-next
   "wh" 'persp-prev
